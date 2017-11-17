c355e332123e02c8ddbc10c930c26fae72fa5daf
Victor victory jaycee <vsquare396@gmail.com>
2017-11-17 21:20:02 +0000

    Initial commit

---
+++ app/src/main/java/bankzworld/com/model/Comment.java
@@ -0,0 +1,28 @@
+package bankzworld.com.model;
+
+/**
+ * Created by King Jaycee on 16/11/2017.
+ */
+
+public class Comment {
+    private long id;
+    private String comment;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getComment() {
+
+        return comment;
+    }
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+    // Will be used by the ArrayAdapter in the ListView
+    @Override
+    public String toString() {
+        return comment;
+    }
+}
